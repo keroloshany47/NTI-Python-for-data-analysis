6b272765ee7837481cf3d78bc400550b1ba76aae
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-05 06:38:27 +0300

    Add files via upload

---
+++ Final Project/convert_movielens.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# ratings.dat
+ratings = pd.read_csv("ratings.dat", sep="::", engine="python", names=["UserID", "MovieID", "Rating", "Timestamp"])
+ratings.to_csv("ratings.csv", index=False)
+
+# users.dat
+users = pd.read_csv("users.dat", sep="::", engine="python", names=["UserID", "Gender", "Age", "Occupation", "Zip-code"])
+users.to_csv("users.csv", index=False)
+
+# movies.dat
+movies = pd.read_csv("movies.dat", sep="::", engine="python", names=["MovieID", "Title", "Genres"])
+movies.to_csv("movies.csv", index=False)
+
+print("✔️ تم تحويل الملفات إلى CSV.")
